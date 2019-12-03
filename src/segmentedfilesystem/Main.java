d1513e2ec6adab20fa81604bb9f91fc73dacffae
xion1547 <xion1547@morris.umn.edu>
2019-12-03 11:28:06 -0600

    implements a template of a QouteClient class. To interact between server and client.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,27 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Main {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        int port;
+        InetAddress address = InetAddress.getByName(args[0]);
+        byte[] buf = new byte[1028];
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
+        socket.send(packet);
+
+        //while() { while the server is sending packets, make a new packet and recieve that packet.
+            packet = new DatagramPacket(buf, buf.length);
+
+        //}
+
+        socket.close();
+
     }
 
 }
