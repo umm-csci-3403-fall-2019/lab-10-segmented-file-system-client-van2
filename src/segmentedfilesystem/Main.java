9bcf965de560f1f6eb322a2d8773e23df18206b4
xion1547 <xion1547@morris.umn.edu>
2019-12-05 11:24:37 -0600

    Adds method for checking if we're done receiving packages.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -9,19 +9,26 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
         int port;
-        InetAddress address = InetAddress.getByName(args[0]);
+        InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
         byte[] buf = new byte[1028];
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
         socket.send(packet);
 
-        //while() { while the server is sending packets, make a new packet and recieve that packet.
+        while(!isDone()) { //while the server is sending packets, make a new packet and recieve that packet.
             packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+        }
 
-        //}
+
 
         socket.close();
 
+    }
+
+    
+    public static boolean isDone(){
+        return true;
     }
 
 }
