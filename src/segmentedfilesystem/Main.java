df184b8c9916ae1e8e3a87daa44ca21e38a51482
JoshuaHamann <haman127@morris.umn.edu>
2019-12-10 11:33:07 -0600

    made the mods for which packets are recieved

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -4,20 +4,33 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         int port;
         InetAddress address = InetAddress.getByName("csci-4409.morris.umn.edu");
         byte[] buf = new byte[1028];
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
+        ArrayList<DatagramPacket> header = new ArrayList<DatagramPacket>();
+        ArrayList<DatagramPacket> datap = new ArrayList<DatagramPacket>();
+                //Starting the Conversation
         socket.send(packet);
+
+        
 
         while(!isDone()) { //while the server is sending packets, make a new packet and recieve that packet.
             packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
+            if(buf[0] % 2  == 0){
+                header.add(packet);
+            } else {
+                datap.add(packet);
+            }
+
         }
 
 
@@ -26,7 +39,6 @@ public class Main {
 
     }
 
-    
     public static boolean isDone(){
         return true;
     }
