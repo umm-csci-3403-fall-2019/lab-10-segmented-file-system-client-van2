167f401ec9d3604769189c031e34b511cc61b481
JoshuaHamann <haman127@morris.umn.edu>
2019-12-12 11:34:56 -0600

    Adds method to add packet to a hash map that sorts by fileID. Attempts to create JUnit tests for addToMap.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,11 +1,16 @@
 package segmentedfilesystem;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -15,20 +20,22 @@ public class Main {
         byte[] buf = new byte[1028];
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6014);
-        ArrayList<DatagramPacket> header = new ArrayList<DatagramPacket>();
-        ArrayList<DatagramPacket> datap = new ArrayList<DatagramPacket>();
-                //Starting the Conversation
+        //Starting the Conversation
         socket.send(packet);
+        //ArrayList<DatagramPacket> File1; //= //new ArrayList<>();
+        //ArrayList<DatagramPacket> File2 = new ArrayList<>();
 
-        
+
+        HashMap<Integer, ArrayList<DatagramPacket>> map = new HashMap<>();
 
         while(!isDone()) { //while the server is sending packets, make a new packet and recieve that packet.
             packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
+            //sortPacket(packet);
             if(buf[0] % 2  == 0){
-                header.add(packet);
+                //header.add(packet); add to an array
             } else {
-                datap.add(packet);
+                addToMap(packet, map);
             }
 
         }
@@ -37,6 +44,27 @@ public class Main {
 
         socket.close();
 
+    }
+
+    public static void addToMap(DatagramPacket myPacket, HashMap<Integer, ArrayList<DatagramPacket>> map){
+        byte[] dataByte = myPacket.getData();
+        int fileID = dataByte[1];
+        if(map.containsKey(fileID)) {
+            map.get(fileID).add(myPacket);
+        } else {
+            ArrayList<DatagramPacket> myList = new ArrayList<>();
+            myList.add(myPacket);
+            map.put(fileID, myList);
+        }
+
+    }
+
+    public static ArrayList<DatagramPacket> sortPacket(DatagramPacket p){
+        byte[] pdata = p.getData();
+        if(pdata[1] % 2 == 0){
+
+        }
+        return null;
     }
 
     public static boolean isDone(){
---
+++ test/MainTest.java
@@ -0,0 +1,15 @@
+import java.net.DatagramPacket;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+
+    public static void main(String[] args) {
+        //DatagramPacket myPacket = new DatagramPacket();
+    }
+    void addToMap() {
+        //DatagramPacket myPacket = new DatagramPacket();
+    }
+}
